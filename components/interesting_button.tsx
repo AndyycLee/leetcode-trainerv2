45b6283ae708cfae68f3c763d8de755ec125c372
Andy <calgarykunhong@gmail.com>
2023-01-03 04:01:45 -0700

    firebase hurts and i initialized it, also content script folder

--- components/interesting_button.tsx
+++ components/interesting_button.tsx
@@ -1,38 +1,55 @@
 import "./interesting_button.css"
-import React from 'react';
+
+import React from "react"
+
+//used to have import googleLogo from "../assets/google-login.svg"
+
 
 type ButtonProps = {
-  elementType: string;
-  children: any;
-  filled?: boolean;
-  secondary?: boolean;
-  onClick?: () => void;
-};
+  elementType: string
+  children: any
+  filled?: boolean
+  secondary?: boolean
+  onClick?: () => void
+}
 
-const Button = ({ elementType, children, filled, secondary, onClick, ...rest }: ButtonProps) => {
+const Button = ({
+  elementType,
+  children,
+  filled,
+  secondary,
+  onClick,
+  ...rest
+}: ButtonProps) => {
   return (
-    React.createElement(elementType, {
-      className: `dir-control ${secondary ? 'dir-control--secondary' : ''} ${filled ? 'dir-control--filled' : ''}`,
-      onClick,
-      ...rest
-    },
-      children,
-      <span/>,
-      <span/>,
-      <span/>,
-      <span/>,
-      <b aria-hidden="true">{children}</b>,
-      <b aria-hidden="true">{children}</b>,
-      <b aria-hidden="true">{children}</b>,
-      <b aria-hidden="true">{children}</b>
-    )
+    <>
+      <div
+        className={`dir-control ${secondary ? "dir-control--secondary" : ""} ${
+          filled ? "dir-control--filled" : ""
+        }`}
+        onClick={onClick}
+        {...rest}>
+
+        {children}
+        <span />
+        <span />
+        <span />
+        <span />
+        <b aria-hidden="true">{children}</b>
+        <b aria-hidden="true">{children}</b>
+        <b aria-hidden="true">{children}</b>
+        <b aria-hidden="true">{children}</b>
+      </div>
+    </>
   )
 }
+
 Button.defaultProps = {
-  elementType: 'button'
+  elementType: "button"
 }
 
-export default Button;
+export default Button
+//        {/* <img src={googleLogo} alt="google-logo" className="google-img" style={{ transform: 'translate(0, calc(var(--y, 0) * 1%)) scale(var(--scale, 1))' }} /> */}
 
 // const Button = ({ as, children, filled, secondary, ...rest }) => {
 //   const that = {
---
+++ firebase_components/firebase_post.tsx
@@ -0,0 +1,27 @@
+import { getFirestore, serverTimestamp } from "firebase/firestore";
+import { app } from "../firebase";
+
+// Get a Firestore instance const db = firebase.firestore()
+
+const db = getFirestore(app);
+
+//exort the db, and then import in popup.tsx, i think next
+//step is to create a new collection in firebase just like thingsRef = db.collection('things');
+
+// import { collection, addDoc } from "firebase/firestore"; 
+
+// try {
+//   const docRef = await addDoc(collection(db, "users"), {
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// } catch (e) {
+//   console.error("Error adding document: ", e);
+// } https://www.youtube.com/watch?v=YPgb7g8is2w
+//it works! now on submit i need to take data from the payload from leetcode and store it in the db, later i can build a ui to display the data
+// let timestamp = serverTimestamp()
+
+export {}
+
