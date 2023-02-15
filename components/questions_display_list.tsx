b49d585423261645a2ab38a78dca5592ef0afa74
Andy <calgarykunhong@gmail.com>
2023-02-15 05:33:29 -0700

    just added a semi colon and spacing correction :)

--- components/questions_display_list.tsx
+++ components/questions_display_list.tsx
@@ -58,10 +58,9 @@ const Questions_display_list = ({ globalUserAuthorized }) => {
   const thingsList = document.getElementById("thingsList")
   //ret is optional, thats the beauty of onAuthStateChanged, it runs immediately if its thedefault tab, otherwise you need UseEffect to wake up the page, and then it will be an observer
   // and it will find that user and then runs again when the auth state changes, so itwakes up and does its run on navigation, and then it runs again when the auth state changes
-//onAuthStateChanged method will run on page load or navigation, in addition to running every time there is a change in the authentication state
+  //onAuthStateChanged method will run on page load or navigation, in addition to running every time there is a change in the authentication state
 
-
-//because my sign in is on a different page, i could have useEffect auth = getAuth()and then render the UI instead of the wakey method of OnAtuhStateChange
+  //because my sign in is on a different page, i could have useEffect auth = getAuth()and then render the UI instead of the wakey method of OnAtuhStateChange
   let ret = onAuthStateChanged(auth, async (user) => {
     if (user) {
       // Show the UI.
@@ -192,7 +191,7 @@ const Questions_display_list = ({ globalUserAuthorized }) => {
 
       {/* <Dropdown></Dropdown> */}
     </div>
-  )
+  );
 }
 
 export default Questions_display_list
